95160f5321450e3f402ba641d73be6706d7f8fea
Yo1 <bigdiebam@gmail.com>
2023-05-04 23:56:17 +0300

    update

--- polls/urls.py
+++ polls/urls.py
@@ -5,14 +5,10 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("home/", views.home, name="home"),
     path("teacher/", views.add_teacher, name="add_teacher"),
-    path(
-        "teacher/edit_delete/<int:id>", views.teacher_edit, name="teacher_edit"
-    ),
+    path("teacher/edit_delete/<int:id>", views.teacher_edit, name="teacher_edit"),
     path("teachers/", views.teacher_list, name="teachers_display"),
     path("student/", views.add_student, name="add_student"),
-    path(
-        "student/edit_delete/<int:id>", views.student_edit, name="student_edit"
-    ),
+    path("student/edit_delete/<int:id>", views.student_edit, name="student_edit"),
     path("students/", views.student_list, name="students_display"),
     path("group/", views.add_group, name="add_group"),
 ]
